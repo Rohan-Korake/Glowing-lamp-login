30d4c0131c97a38dc986880108bbcf6fe40a0b64
Rohan Korake <rohannkorake@gmail.com>
2026-04-14 14:22:29 +0530

    replace .then with async await

--- src/login.js
+++ src/login.js
@@ -8,50 +8,52 @@ import {
 import { showLoader, hideLoader } from "./main.js";
 
 export function userLogin() {
-  document.getElementById("loginForm").addEventListener("submit", (e) => {
+  document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault();
     showLoader();
     const userName = document.getElementById("userName").value;
     const password = document.getElementById("password").value;
-    fetch("https://authentication-service-vdxw.onrender.com/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: userName,
-        password: password,
-      }),
-    })
-      .then((response) => {
-        return response.json().then((data) => {
-          hideLoader();
-          return {
-            status: response.status,
-            body: data,
-          };
-        });
-      })
-      .then((result) => {
-        const { status, body } = result;
 
-        switch (status) {
-          case 400:
-          case 401:
-          case 403:
-          case 500:
-            showError("loginFormError", body.message);
-            break;
+    try {
+      const response = await fetch(
+        "https://authentication-service-vdxw.onrender.com/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: userName,
+            password: password,
+          }),
+        },
+      );
 
-          case 200:
-            hideError("loginFormError");
-            hideElement("loginPage");
-            showElement("welcomePage");
-            break;
+      const data = await response.json();
+      hideLoader();
+      const status = response.status;
+      const body = data;
 
-          default:
-            showError("loginFormError", "Unexpected error");
-        }
-      });
+      switch (status) {
+        case 400:
+        case 401:
+        case 403:
+        case 500:
+          showError("loginFormError", body.message);
+          break;
+
+        case 200:
+          hideError("loginFormError");
+          hideElement("loginPage");
+          showElement("welcomePage");
+          break;
+
+        default:
+          showError("loginFormError", "Unexpected error");
+      }
+    } catch (error) {
+      hideLoader();
+      showError("loginFormError", "Network error");
+    }
   });
 }
